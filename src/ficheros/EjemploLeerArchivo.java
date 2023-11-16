61f146d23d29c952ed286bec7c8d1924b5176c4e
César Antonio <cesarfrm5@gmail.com>
2023-11-16 20:46:07 +0100

    actualizacion

---
+++ src/ficheros/EjemploLeerArchivo.java
@@ -0,0 +1,13 @@
+package ficheros;
+
+import java.io.IOException;
+
+public class EjemploLeerArchivo {
+    public static void main(String[] args) {
+        //String nombreArchivo = "D:\\Cursos\\Java\\archivo\\java.txt";
+    	String nombreArchivo = "./src/ficheros/directorios2";
+        ArchivoServicio servicio = new ArchivoServicio();
+
+      
+    }
+}
---
+++ src/ficheros/ejercicios/GestionFicheros.java
@@ -0,0 +1,76 @@
+package ficheros.ejercicios;
+
+import java.io.File;
+import java.time.LocalDateTime;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class GestionFicheros {
+	private static final Logger logger = LogManager.getLogger(GestionFicheros.class);
+	private int contadorJPGs;
+	
+	public static void main(String[] args) {
+		GestionFicheros gestionFicheros = new GestionFicheros();
+		gestionFicheros.empiezaEjercicio();
+
+	}
+	
+	public void empiezaEjercicio() {
+		File dir = new File("./enunciados/ficheros/directorios2");
+		//contarJPGs(dir);
+		//logger.info("Hay un total de " + this.contadorJPGs + " fichero .jpg");
+		LocalDateTime datetime = LocalDateTime.now();
+		
+		mostrarNombreFicheroAnioActual(dir, String.valueOf(datetime.getYear() +1), String.valueOf(datetime.getMonthValue()));
+		
+	}
+	
+	public void contarJPGs (File directorio) {
+		logger.debug("Buscando jpgs en directorio "+ directorio.getAbsolutePath());
+		File[] files = directorio.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				contarJPGs(file);
+			}else {
+				logger.debug("Encontrado un fichero");
+				if(file.getName().endsWith(".jpg")) {
+					logger.debug("es un jpg");
+					contadorJPGs++;
+				}
+			}
+		}
+	}
+	
+	public void mostrarNombreFicheroAnioActual (File directorio, String anioActual, String mes) {
+//		logger.debug("Buscando ficheros con el año actual "+ anioActual +
+//				" en el nombre dentro del directorio "+directorio.getAbsolutePath() );
+//		
+		File[] files = directorio.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				mostrarNombreFicheroAnioActual(file,anioActual, mes);
+			}else {
+				logger.debug("Encontrado un fichero");
+				if(file.getName().contains(anioActual) && file.getName().contains(mes)) {
+					logger.debug("Encontrado un fichero con el año + " + anioActual + " y el mes " + mes);
+					
+				}
+			}
+		}
+	}
+	
+	private void printInfoFile(File fichero) {
+		logger.info("Ruta: "+fichero.getAbsolutePath());
+		logger.info("Tamaño: "+fichero.length());
+		logger.info("Oculto: "+fichero.isHidden());
+		logger.info("Se puede ejecutar: "+fichero.canExecute());
+		logger.info("Se puede leer: "+fichero.canRead());
+		logger.info("Se puede escribir: "+fichero.canWrite());
+		
+	}
+	
+
+
+}
+
